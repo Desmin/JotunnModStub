dc51e1315fc2888a10e8cb1eed25f1e6b6b0aaf8
Desmin <deslittle2015@gmail.com>
2022-02-09 18:25:05 -0500

    Starting work on the container cache and util class.

---
+++ VikingrStorage/Util/ContainerCache.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VikingrStorage.Util
+{
+    public class ContainerCache
+    {
+        private List<Container> Containers { get; set; }
+
+        public ContainerCache()
+        {
+            Containers = InitializeCache();
+        }
+
+        private List<Container> InitializeCache()
+        {
+            return null;
+        }
+    }
+}
